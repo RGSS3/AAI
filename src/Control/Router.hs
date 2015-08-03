5005bdabf3d35fdeaf693e3403ba2fa1a613d8ee
Nils 'bash0r' Jonsson <aka.bash0r@gmail.com>
2015-08-03 18:12:42 +0200

    Implemented basic routing mechanism.

--- src/Control/Router.hs
+++ src/Control/Router.hs
@@ -13,11 +13,32 @@ A simple type class implementing a simple routing table for commands.
 -}
 module Control.Router
 ( Router (..)
+, ErrorHandler
+, route
 ) where
 
+import Control.Applicative
 import Control.Command
+import Control.Monad
 
 
 class (Command a) => Router a where
     routes :: a -> String -> Bool
 
+-- | A simple error handling function.
+type ErrorHandler = String -> IO ()
+
+-- | Route a specific instruction to a command.
+route :: (Command a, Router a) => ErrorHandler -> [a] -> [String] -> IO ()
+route eh cmds  []        = eh "Empty argument list. Cannot route command."
+route eh cmds (cmd:args) =
+    case findCommand cmd cmds of
+      Just a  -> execute a args
+      Nothing -> eh ("Command " ++ cmd ++ " is unknown.")
+  where
+    findCommand cmd (x:xs) =
+      if routes x cmd
+         then return x
+         else findCommand cmd xs
+    findCommand _   []     =
+      empty
