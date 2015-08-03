41e0f8c926c585aa5ea719d7ac955b235dab322f
Nils 'bash0r' Jonsson <aka.bash0r@gmail.com>
2015-08-03 18:39:04 +0200

    Fixed pattern definition of findCommand and finished documentation.

--- src/Control/Router.hs
+++ src/Control/Router.hs
@@ -21,8 +21,9 @@ import Control.Applicative
 import Control.Command
 import Control.Monad
 
-
+-- | A type class describing basing routing table.
 class (Command a) => Router a where
+    -- | Checks if a command is routable to a specific command name.
     routes :: a -> String -> Bool
 
 -- | A simple error handling function.
@@ -30,15 +31,16 @@ type ErrorHandler = String -> IO ()
 
 -- | Route a specific instruction to a command.
 route :: (Command a, Router a) => ErrorHandler -> [a] -> [String] -> IO ()
-route eh cmds  []        = eh "Empty argument list. Cannot route command."
+route eh cmds []         = eh "Empty argument list. Cannot route command."
 route eh cmds (cmd:args) =
     case findCommand cmd cmds of
       Just a  -> execute a args
       Nothing -> eh ("Command " ++ cmd ++ " is unknown.")
   where
+    findCommand _   []     =
+      empty
     findCommand cmd (x:xs) =
       if routes x cmd
          then return x
          else findCommand cmd xs
-    findCommand _   []     =
-      empty
+
