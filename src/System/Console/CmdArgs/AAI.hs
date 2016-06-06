aa325d3176277d79cf2a00be8fef9b625ade8858
Nils 'bash0r' Jonsson <aka.bash0r@gmail.com>
2016-06-06 21:56:45 +0200

    Implemented basic section command.

--- src/System/Console/CmdArgs/AAI.hs
+++ src/System/Console/CmdArgs/AAI.hs
@@ -1,18 +1,39 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module System.Console.CmdArgs.AAI
     ( Desc
     , AAI (..)
+
+    , section
     ) where
 
 import           Control.Applicative
 import           Control.Monad       (join)
 
+import           Data.Defaultable
+import           Data.Foldable       (asum)
+
 
 type Desc = Int -> String
+
+
+instance Show (Int -> String) where
+  show f = show $ f 0
 
 
 newtype AAI a = AAI
   { runAAI :: [String] -> [(a,Int,Desc,[String],[String])]
   }
+
+
+section :: (Defaultable d) => String -> [AAI d] -> AAI d
+section n ds = AAI $ \as -> case as of
+  a:as' ->
+    if   a == n
+    then map (\(x,i,d,pi,rs) -> (x,i,d,a:pi,rs)) (asum $ map (`runAAI` as') ds)
+    else map (\(x,i,d,pi,rs) -> (x,i+5,d,a:pi,rs)) (asum $
+           map (`runAAI` as) ds ++ map (`runAAI` as') ds)
+  _ -> [(def,10,const "",[n],[])]
 
 
 instance Functor AAI where
@@ -22,7 +43,7 @@ instance Applicative AAI where
   pure a = AAI $ \as -> [(a,0,const "",[],as)]
 
   f <*> a = AAI $ \as ->
-    join $ map (\(g,i,d,pi,as') ->
+    asum $ map (\(g,i,d,pi,as') ->
       map (\(a,i',d',pi',as'') ->
         (g a,i+i',\i -> d i ++ d' i,pi ++ pi',as''))
         (runAAI a as')) (runAAI f as)
